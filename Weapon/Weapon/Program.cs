a389628cf1330ae9e46feb7a8a17ff914f9f25f1
dexSamir <samirah-bp215@code.edu.az>
2024-10-18 04:06:37 -0800

    feat: create Weapon class and add some methods like reaload showfullinfo etc.

---
+++ Weapon/Weapon/Program.cs
@@ -0,0 +1,140 @@
+﻿namespace Weapon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Weapon AKM = new Weapon("AK-47", 40, 23, false);
+            bool firetype = AKM.FireType; 
+            bool isFalse = false;
+            int opt; 
+            int choise;
+
+            do
+            {
+                Console.WriteLine("Etmek istediyiniz emeliyyati secin:");
+                Console.WriteLine("1. Silah haqqinda bilgi elde etmek ucun \n2. Ates acmag ucun \n3. Daragi gulle ile doldurmag ucun ne qeder gulleye ehtiyac olduguna baxin \n4. Daragi yenileyin \n5. Silah Atis modunu deyisin \n6. Edit \n0. EXIT");
+
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (int.TryParse(input, out opt))
+                    {
+                        break; 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Reqem daxil edin");
+                    }
+                }
+                switch (opt)
+                {
+                    case 0:
+                        isFalse = true;
+                        break;
+                    case 1:
+                        AKM.showFullInfo();
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        AKM.Shoot();
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        Console.WriteLine(AKM.GetRemainBulletCount() + " gulle qalib");
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        AKM.Reload();
+                        Console.WriteLine();
+                        break;
+
+                    case 5:
+                        if (firetype)
+                        {
+                            Console.WriteLine($"hazirda silah Avtomatik moddadir \nSilahin modunu deyismek isteyirsiniz mi? \ndeyismek ucun 1-e, imtina etmek ucun 0-a basin");
+                            int change; 
+                            change = int.Parse(Console.ReadLine());
+                            if(change == 0)
+                            {
+                                firetype = true;
+                                Console.WriteLine("Silah modu avtomatikdir");
+                            }
+                            else
+                            {
+                                firetype = false;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"hazirda silah Tekli moddadir \nSilahin modunu deyismek isteyirsiniz mi? \ndeyismek ucun 1-e, imtina etmek ucun 0-a basin");
+                            int change;
+                            change = int.Parse(Console.ReadLine());
+                            if (change == 0)
+                            {
+                                firetype = false;
+                                Console.WriteLine("Silah modu Teklidedir");
+                            }
+                            else
+                            {
+                                firetype = true;
+                            }
+                        }
+                        AKM.ChangeFireMode();
+                        Console.WriteLine();
+                        break;
+                    case 6:
+                        Console.WriteLine("7. Gulle daraginin tutumunu deyisin \n8. Gulle sayini deyisin \n 9. Menu-a qayidin");
+                        bool isEdit = false;
+                        int op; 
+                        do
+                        {
+                            while (true)
+                            {
+                                string input = Console.ReadLine();
+                                if (int.TryParse(input, out op))
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Reqem daxil edin");
+                                }
+                            }
+
+                            switch (op)
+                            {
+                                case 9:
+                                    isEdit = true;
+                                    break;
+                                case 8:
+                                    Console.WriteLine("Yeni gulle sayini daxil edin: ");
+                                    int bullet = int.Parse(Console.ReadLine());
+                                    AKM.changeMagazine(bullet);
+                                    break;
+                                case 7:
+                                    Console.WriteLine("Yeni gulle tutumunu daxil edin: ");
+                                    int magazine = int.Parse(Console.ReadLine());
+                                    AKM.changeCapacity(magazine);
+                                    break;
+                                default:
+                                    Console.WriteLine("Yanlis secim, yeniden yoxla.");
+                                    Console.WriteLine();
+                                    break;
+                            }
+                        }
+                        while (!isEdit);
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Yanlis secim, yeniden yoxla.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            while (!isFalse);
+        }
+    }
+}
---
+++ Weapon/Weapon/Weapon.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon
+{
+    internal class Weapon
+    {
+        public string name; 
+        private int _magazineCapacity;
+        private int _magazine;
+        private bool _fireType;
+
+        public int MagazineCapacity 
+        {
+            get
+            {
+                return _magazineCapacity;
+            }
+            set
+            {
+                if(value> 0)
+                {
+                    _magazineCapacity = value;
+                }
+            }
+        }
+        public int Magazine
+        {
+            get
+            {
+                return _magazine;
+            }
+            set
+            {
+                if (value <= MagazineCapacity)
+                {
+                    _magazine = value;
+                }
+            }
+        }
+
+        public bool FireType
+        {
+            get
+            {
+                return _fireType;
+            }
+            set
+            {
+                _fireType = value;
+            }
+        }
+        public Weapon(string name, int magazineCapacity, int magazine, bool firetype)
+        {
+            MagazineCapacity = magazineCapacity;
+            Magazine = magazine;
+            FireType = firetype;
+            this.name = name;   
+        }
+
+        public void Shoot()
+        {
+            if (Magazine > 0)
+            {
+                if (!FireType)
+                {
+                    Magazine--;
+                }
+                else
+                {
+                    Magazine = 0;
+                }
+                Console.WriteLine($"Ates acildi. \nSilahda {Magazine} sayda gulle qaldi");
+            }
+            else
+            {
+                Console.WriteLine("Balansinizda kifayet qeder gulle qalmiyib. xahis olunur balansinizi artirin)"); 
+            }
+        }
+        public void changeCapacity(int value)
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Silahin gulle tutumu deyisdirilmedi");
+            }
+            else if (value == Magazine) {
+                Console.WriteLine("Deyismek istediyiniz darag ile yeni darag eyni oldugu ucun deyisdirilmedi");
+            }
+            else
+            {
+
+                Console.WriteLine($"Silahin gulle tutumu {MagazineCapacity}-den {value}-a deyisdirildi");
+                MagazineCapacity = value;
+            }
+
+        }
+        public void changeMagazine(int value)
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Silahin gulle sayi deyisdirilmedi");
+            }
+            else if (value > Magazine)
+            {
+                Console.WriteLine("Bu gulle sayi daraga uygun deyil ");
+            }
+            else
+            {
+
+                Console.WriteLine($"Silahin gulle sayi {Magazine}-den {value}-a deyisdirildi");
+                MagazineCapacity = value;
+            }
+
+        }
+        public int GetRemainBulletCount()
+        {
+            return MagazineCapacity - Magazine; 
+        }
+        public void Reload()
+        {
+            Magazine = MagazineCapacity;
+            Console.WriteLine("Gulle yeniden dolduruldu");
+        }
+        public void ChangeFireMode()
+        {
+            bool first = FireType;
+            if (FireType)
+            { 
+                FireType = false;
+                Console.WriteLine($"Atis modu avtomatik-den tekli moda deyisdirildi");
+            }
+            else
+            {
+                FireType = true;
+                Console.WriteLine($"Atis modu tekli moddan avtomatik moda deyisdirildi");
+            }
+        }
+        public void showFullInfo()
+        {
+            Console.WriteLine($"Name: {name} \nDaragin gulle tutumu: {MagazineCapacity} \nDaragda ki gulle sayi: {Magazine}");
+            if (FireType)
+            {
+                Console.WriteLine("Silahin Atis modu: Avtomatik");
+            }
+            else 
+            {
+                Console.WriteLine("Silahin Atis modu: Tekli");
+            }
+        } 
+    }
+}
